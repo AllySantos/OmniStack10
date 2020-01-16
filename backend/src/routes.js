303ec69c881b7c490f1ef8bfca609a5b061897cb
ALICIA RDRIGUES DOS SANTOS <alicia.santos01@fatec.sp.gov.br>
2020-01-15 22:23:11 -0200

    Cadastra localização e arq movidos para controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,30 +2,8 @@ const { Router } = require('express')
 const axios = require('axios')
 
 const Dev = require('./models/Dev')
-
+const DevController = require('./controllers/DevController')
 const routes = Router()
 
-routes.post('/devs', async (req, res) => {
-
-    const { github_username, techs } = req.body
-
-    const response  = await axios.get(`https://api.github.com/users/${ github_username }`)
-    
-    const {name = login, avatar_url, bio}  = response.data
-
-   const techsArray = techs.split(',').map(tech =>  tech.trim())
-
-   const devResposta = await Dev.create({
-      name,
-       github_username,
-       bio,
-       avatar_url,
-       techs: techsArray
-
-   })
-
-    return res.json(devResposta)
-})
-
-
+routes.post('/devs', DevController.store)
 module.exports = routes
