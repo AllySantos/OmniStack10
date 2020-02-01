4ad732f2017a5fbd670b3d0c933a6d51f0a97c09
ALICIA RDRIGUES DOS SANTOS <alicia.santos01@fatec.sp.gov.br>
2020-02-01 17:06:53 -0300

    vou att o expo e pode dar ruim

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bom dia </Text>
     </View>
   );
 }
@@ -12,8 +12,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#FFF',
+    textAlign: 'center',
+
+  }
 });
---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Main(){
+    <View />
+}
+
+export default Main;
+
