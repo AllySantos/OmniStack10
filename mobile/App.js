52e4aa350e520544bfc9371b62c0798592d9b2b4
ALICIA RDRIGUES DOS SANTOS <alicia.santos01@fatec.sp.gov.br>
2020-02-05 18:23:40 -0300

    My message

--- mobile/App.js
+++ mobile/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+
+import Routes from './src/routes';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bom dia </Text>
-    </View>
-  );
+
+    return (
+      <>
+       <StatusBar />
+      <Routes></Routes>
+    
+      </>
+
+       
+    );
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#FFF',
-    textAlign: 'center',
-
-  }
-});
