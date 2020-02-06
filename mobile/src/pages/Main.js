9d833e1f8f1b90e93fc5a5e414c71b73d9679343
ALICIA RDRIGUES DOS SANTOS <alicia.santos01@fatec.sp.gov.br>
2020-02-05 21:38:16 -0300

    App com a estilização pronta

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import {getCurrentPositionAsync, requestPermissionsAsync} from 'expo-location'
-import MapView from 'react-native-maps'
-
-function Main(){
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { getCurrentPositionAsync, requestPermissionsAsync } from 'expo-location'
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { MaterialIcons } from '@expo/vector-icons'
+function Main({ navigation }){
 
     const [currentRegion, setCurrentRegion] = useState(null)
 
@@ -33,12 +33,106 @@ function Main(){
     if(!currentRegion){
         return null
     }
-    return <MapView initialRegion={currentRegion} style={styles.map}/>
+    return(
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{latitude: -23.550431, longitude: -46.461963}}>
+                <Image style={styles.avatar}  source={{uri: 'https://avatars3.githubusercontent.com/u/4248081?s=460&v=4'}}/>
+            
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', {github_username: 'filipedeschamps'})
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Felipe Deschamps</Text>
+                        <Text style={styles.devBio}>A nice guy.</Text>
+                        <Text style={styles.devTechs}>React, ReactJS, ReactNative</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar por techs"
+                    placeholderTextColor= "#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={()=>{}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
+
+    ) 
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+
+    avatar: {
+        width:54,
+        height:54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+
+    callout: {
+        width:260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+
+    devBio: {
+        marginTop: 5,
+        color: '#666'
+    },
+
+     devTechs: {
+         marginTop: 5,
+     },
+
+     searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 })
 
